8d6fc047902749db67e6363a9abb596c11174166
SON <wjdgh2777@naver.com>
2022-07-04 01:51:13 +0900

    Feat: Sequelize초기설정 최종

--- Back_End/controllers/db.js
+++ Back_End/controllers/db.js
@@ -4,19 +4,13 @@ const { User } = require('../src/utils/connect');
 
 const output = {
     user_info : async (req, res) => {
-        const user_info = await User.findAll();
-        const result = [];
-
-        for (const user of Object.values(user_info)) { // values값들만 (errors is not iterable 해결)
-            result.push({
-            nickname: user.nickname,
-            id: user.id,
-            psword: user.psword,
-            email: user.email
+        await User.findAll()
+            .then((response) => {
+                res.send(response);
             })
-        }
-
-        res.send(result);
+            .catch((error) => {
+                console.log(error);
+            });
     },
 };
 
