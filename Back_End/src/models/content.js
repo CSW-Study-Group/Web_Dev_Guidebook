c2e7da39fe224eab19b8a9c080748aa91014d4e6
SON <wjdgh2777@naver.com>
2022-07-27 22:10:32 +0900

    Edit: 모델 & 연결수정

--- Back_End/src/models/content.js
+++ Back_End/src/models/content.js
@@ -36,15 +36,12 @@ module.exports = class Content extends Sequelize.Model {
                 view: {
                     type: Sequelize.INTEGER,
                     allowNull: false,
-                },
-                deleted: {
-                    type: Sequelize.BOOLEAN,
-                    allowNull: false,
                 }
             },
             {  // 테이블 자체에 대한 설정
                 sequelize, /* static init 메서드의 매개변수와 연결되는 옵션으로, db.sequelize 객체를 넣어야 한다. */
                 timestamps: true, /* true : 각각 레코드가 생성, 수정될 때의 시간이 자동으로 입력된다. */
+                paranoid: true,
                 modelName: 'Content', /* 모델 이름을 설정. */
                 tableName: 'contentinfo', /* 데이터베이스의 테이블 이름. */
                 charset: 'utf8', /* 인코딩 */
--- Back_End/src/utils/connect.js
+++ Back_End/src/utils/connect.js
@@ -3,6 +3,7 @@
 const Sequelize = require('sequelize');
 const config = require('config');
 const User = require('../models/user');
+const Content = require('../models/content');
 
 // new Sequelize를 통해 MySQL 연결 객체를 생성한다.
 const sequelize = new Sequelize(
@@ -16,7 +17,9 @@ const db = {};
 
 db.sequelize = sequelize;
 db.User = User;
+db.Content = Content;
 
 User.init(sequelize);
+Content.init(sequelize);
 
 module.exports = db;
