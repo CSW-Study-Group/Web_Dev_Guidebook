645b45fa7ae474d67c6aae32fde57b741f0227ff
SON <wjdgh2777@naver.com>
2022-07-28 00:21:50 +0900

    Test: 테스트코드 임시 주석처리

--- Back_End/src/routes/board.js
+++ Back_End/src/routes/board.js
@@ -5,7 +5,7 @@ const ctrl = require('../controllers/board');
 
 const router = express.Router();
 
-router.post("/content", ctrl.content);
-router.get("/content", ctrl.content_g)
+// router.post("/content", ctrl.content);
+// router.get("/content", ctrl.content_g)
 
 module.exports = router;
