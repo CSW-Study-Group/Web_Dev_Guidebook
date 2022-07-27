264363c65a53f836c7106dff501ee6e4f68f1639
pyola4981 <pyola4981@naver.com>
2022-07-27 21:20:21 +0900

    Feat: content 모델 추가

--- Back_End/src/models/content.js
+++ Back_End/src/models/content.js
@@ -2,7 +2,7 @@
 
 const Sequelize = require('sequelize');
 
-module.exports = class User extends Sequelize.Model {
+module.exports = class Content extends Sequelize.Model {
     static init(sequelize) {
         return super.init(
             {  // 테이블 필드에 대한 설정
@@ -13,24 +13,40 @@ module.exports = class User extends Sequelize.Model {
                     unique: true, // 중복 X
                     primaryKey: true,
                 },
-                username: {
-                    type: Sequelize.STRING(30),
+                title: {
+                    type: Sequelize.STRING(255),
                     allowNull: false,
                 },
-                email: {
-                    type: Sequelize.STRING(30),
+                content: {
+                    type: Sequelize.STRING(255),
                     allowNull: false,
                 },
-                password: {
-                    type: Sequelize.STRING(100),
+                tag: {
+                    type: Sequelize.STRING(255),
+                    allowNull: false,
+                },
+                stack: {
+                    type: Sequelize.STRING(255),
+                    allowNull: false,
+                },
+                hit: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                },
+                view: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                },
+                deleted: {
+                    type: Sequelize.BOOLEAN,
                     allowNull: false,
                 }
             },
             {  // 테이블 자체에 대한 설정
                 sequelize, /* static init 메서드의 매개변수와 연결되는 옵션으로, db.sequelize 객체를 넣어야 한다. */
-                timestamps: false, /* true : 각각 레코드가 생성, 수정될 때의 시간이 자동으로 입력된다. */
-                modelName: 'User', /* 모델 이름을 설정. */
-                tableName: 'userinfo', /* 데이터베이스의 테이블 이름. */
+                timestamps: true, /* true : 각각 레코드가 생성, 수정될 때의 시간이 자동으로 입력된다. */
+                modelName: 'Content', /* 모델 이름을 설정. */
+                tableName: 'contentinfo', /* 데이터베이스의 테이블 이름. */
                 charset: 'utf8', /* 인코딩 */
                 collate: 'utf8_general_ci'
             }
