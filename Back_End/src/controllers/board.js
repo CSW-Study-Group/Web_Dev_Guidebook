fd350f7bced6d7da0f59748b278c54cb6a3f5add
SON <wjdgh2777@naver.com>
2022-07-28 22:25:22 +0900

    Feat: 오류출력부분 추가

--- Back_End/src/controllers/board.js
+++ Back_End/src/controllers/board.js
@@ -4,26 +4,33 @@ const { Content } = require('../utils/connect');
 const { User } = require('../utils/connect');
 
 exports.content = (req, res, next) => {
-    let { title, content, tag, stack, hit, view } = req.body;
+    let { title, content, tag, stack } = req.body;
     let userkey = req.decoded.id;
 
-    Content.create({
-        title: title,
-        content: content,
-        tag: tag,
-        stack: stack,
-        hit: hit,
-        view: view,
-        userkey: userkey
-    }).then((data) => {
-        return res.status(200).json({
-            data
+    // 사용자가 title, content입력 안할시 오류 발생
+    if(!title || !content) {
+        return res.status(400).json({
+            message: "Please enter title or content.",
         });
-    }).catch((err) => {
-        return res.status(500).json({
-            err
+    } else {
+        Content.create({
+            title: title,
+            content: content,
+            tag: tag,
+            stack: stack,
+            hit: 0,
+            view: 0,
+            userkey: userkey
+        }).then((data) => {
+            return res.status(200).json({
+                data
+            });
+        }).catch((err) => {
+            return res.status(500).json({
+                err
+            });
         });
-    });
+    }
 };
 
 exports.contentByid = (req, res, next) => {
