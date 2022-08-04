50068ce0af24500c674595ff23c5f9905fe5e991
choihb <chb0127@naver.com>
2022-08-05 00:27:56 +0900

    Feat: 전체검색 기능 추가

--- Back_End/src/routes/board.js
+++ Back_End/src/routes/board.js
@@ -9,4 +9,6 @@ const router = express.Router();
 router.post("/content", verifyJWT, ctrl.content);
 router.get("/content/:userid", ctrl.contentByid) // 해당 유저가 작성한 글 제공
 
+router.get("/searchAll",ctrl.searchAll);//전체검색
+
 module.exports = router;
