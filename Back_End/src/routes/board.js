c0aa53ca5905b81433eb6b115aec35eb2b5796a6
pyola4981 <pyola4981@naver.com>
2022-08-10 13:22:59 +0900

    Feat: 스택 검색 api, 정렬 기능 추가

--- Back_End/src/routes/board.js
+++ Back_End/src/routes/board.js
@@ -7,10 +7,11 @@ const verifyJWT = require('../middleware/verifyJWT');
 const router = express.Router();
 
 router.get("/content/userid/:userid", ctrl.contentGetById) // 해당 유저가 작성한 글 제공
-router.get("/content/stack/:stack", ctrl.contentGetByStack);
+router.get("/content/stack", ctrl.contentGetByStack);
 
 router.post("/content", verifyJWT, ctrl.contentPost);
 
-router.get("/searchall/:type/:content", ctrl.searchAll); // 전체검색
+router.get("/content/searchall", ctrl.searchAll); // 전체검색
+router.get("/content/stack/:stack/searchstack", ctrl.searchStack); // 스택검색
 
 module.exports = router;
