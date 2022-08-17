4a3945b273b3c98b15688a24214fcf5ea941f24d
SON <wjdgh2777@naver.com>
2022-08-18 01:14:16 +0900

    Feat: CRUD의 RUD & JWT인증 & 작성자매칭 API추가

--- Back_End/src/middleware/verifyJWT.js
+++ Back_End/src/middleware/verifyJWT.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 const ACCESS_SECRET_KEY = config.get('JWT.access_secret_key');
 
-const verifyJWT = (req, res, next) => {
+exports.verifyJwtAndNext = (req, res, next) => { // 미들웨어 2개 이상
     try {
         req.decoded = jwt.verify(req.headers.authorization, ACCESS_SECRET_KEY);
         return next();
@@ -27,4 +27,30 @@ const verifyJWT = (req, res, next) => {
     }
 };
 
-module.exports = verifyJWT;
+exports.verifyJwt = (req, res, next) => { // 미들웨어 1개 한정
+    try {
+        if (jwt.verify(req.headers.authorization, ACCESS_SECRET_KEY)) {
+            return res.status(200).json({
+                code: 200,
+                message: "Token verify success."
+            });
+        }
+    }
+    catch (error) {
+        // 유효시간이 초과된 경우
+        if (error.name === "TokenExpiredError") {
+            return res.status(419).json({
+                code: 419,
+                message: "Token has expired."
+            });
+        }
+        // 토큰의 비밀키가 일치하지 않는 경우
+        if (error.name === "JsonWebTokenError") {
+            return res.status(401).json({
+                code: 401,
+                message: "Invalid token."
+            });
+        }
+    }
+};
+
--- Back_End/src/routes/board.js
+++ Back_End/src/routes/board.js
@@ -2,16 +2,18 @@
 
 const express = require('express');
 const ctrl = require('../controllers/board');
-const verifyJWT = require('../middleware/verifyJWT');
 
 const router = express.Router();
-
-router.get("/content/userid/:userid", ctrl.contentGetById) // 해당 유저가 작성한 글 제공
 
 router.get("/content/stack/:stack", ctrl.contentGetByStack); // 스택
 router.get("/content/stack/:stack/search", ctrl.searchPart); // 스택 - 부분검색
 router.get("/content/searchall", ctrl.searchAll); // 전체검색
 
-router.post("/content", verifyJWT, ctrl.contentPost);
+router.get("/content/:id", ctrl.contentRead); // Read
+router.post("/content", ctrl.contentPost); // Create
+router.post("/content/updation/:id", ctrl.contentUpdate); // Update
+router.post("/content/deletion/:id", ctrl.contentDelete); // Delete
+
+router.get("/content/:id/auth/user/:userid", ctrl.auth) // 글 작성자 <-(matching)-> 현재 사용자
 
 module.exports = router;
