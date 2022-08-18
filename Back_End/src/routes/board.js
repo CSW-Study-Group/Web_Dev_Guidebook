01bc508ae3d13f0f61ab03567b302bd067a46c8b
SON <wjdgh2777@naver.com>
2022-08-18 23:05:39 +0900

    Edit: 코드수정

--- Back_End/src/routes/board.js
+++ Back_End/src/routes/board.js
@@ -9,12 +9,12 @@ router.get("/content/stack/:stack", ctrl.contentGetByStack); // 스택
 router.get("/content/stack/:stack/search", ctrl.searchPart); // 스택 - 부분검색
 router.get("/content/searchall", ctrl.searchAll); // 전체검색
 
-router.get("/content/:id", ctrl.contentRead); // Read
+router.get("/content/:contentid", ctrl.contentRead); // Read
 router.post("/content", ctrl.contentPost); // Create
-router.post("/content/updation/:id", ctrl.contentUpdate); // Update
-router.post("/content/deletion/:id", ctrl.contentDelete); // Delete
+router.post("/content/updation/:contentid", ctrl.contentUpdate); // Update
+router.post("/content/deletion/:contentid", ctrl.contentDelete); // Delete
 
-router.get("/content/:id/auth/user/:userid", ctrl.auth) // 글 작성자 <-(matching)-> 현재 사용자
+router.get("/content/:contentid/auth/user/:userid", ctrl.auth) // 글 작성자 <-(matching)-> 현재 사용자
 
 router.post("/content/:contentid/comment", verifyJWT.verifyJwtAndNext, ctrl.commentPost);
 
